ac075f2cd953bad4b3d408f736633ace04f352a5
yaminiudata135791 <yaminiudata@gmail.com>
2022-04-25 18:16:35 +0530

    "Adding two numbers"

---
+++ banking-app/src/main/java/com/dlithe/bankingapp/practice/Demo.java
@@ -0,0 +1,13 @@
+package com.dlithe.bankingapp.practice;
+/**
+ * @author Yamini Udata
+ * @version 1.0
+ *
+ */
+public class Demo {
+    public void addNumbers(int i,int j){
+        i=5;
+        j=10;
+        System.out.println(i+j);
+    }
+}
